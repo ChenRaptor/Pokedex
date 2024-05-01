3aae4635221ea409df8dd9ca9ac55ead7c64176c
Chen <chenraptor@outlook.fr>
2024-05-01 21:34:21 +0200

    base2

--- src/pkdxapi/src/main/java/fr/iut/ab/pkdxapi/configurations/SecurityConfiguration.java
+++ src/pkdxapi/src/main/java/fr/iut/ab/pkdxapi/configurations/SecurityConfiguration.java
@@ -4,11 +4,8 @@ import org.springframework.security.config.Customizer;
 
 import javax.crypto.spec.SecretKeySpec;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,7 +18,6 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -32,24 +28,14 @@ import com.nimbusds.jose.jwk.source.ImmutableSecret;
 public class SecurityConfiguration {
 
 	@Bean
-	public SecurityFilterChain filterChainAuthBasic(HttpSecurity http) throws Exception {
-		return http
-				.csrf(csrf -> csrf.disable())
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(auth -> auth.antMatchers(HttpMethod.GET, "/api/pokemons/**").hasRole("USER"))
-				.httpBasic(Customizer.withDefaults())
-				.build();
-	}
-
-  @Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
-				.csrf(csrf -> csrf.disable())
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-				.httpBasic(Customizer.withDefaults())
-        .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
-				.build();
+			.csrf(csrf -> csrf.disable())
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+			.httpBasic(Customizer.withDefaults())
+			.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+			.build();
 	}
 
 	@Bean
@@ -64,15 +50,15 @@ public class SecurityConfiguration {
 		return new BCryptPasswordEncoder();
 	}
 
-  private String jwtKey = "k583to182ms8h53k9oltqtem101sg20m0nxmxy5b50t05wj137257b8apm4c8dl7ybzh9iy0eqv69278z6o0400808qq67z5o1nqkgh7biho85479949a4t4mrsf0p3lfre06v9168yt84n23u91r4bk9r77p470ivj8y0ajm0r57xsnqq8w92bq9i3z4w3xi894h5nj55a7n6m5b4a6215q0k4wkhd7ua26oj882253jr4c13k2pe3je18sem69";
+	private String jwtKey = "k583to182ms8h53k9oltqtem101sg20m0nxmxy5b50t05wj137257b8apm4c8dl7ybzh9iy0eqv69278z6o0400808qq67z5o1nqkgh7biho85479949a4t4mrsf0p3lfre06v9168yt84n23u91r4bk9r77p470ivj8y0ajm0r57xsnqq8w92bq9i3z4w3xi894h5nj55a7n6m5b4a6215q0k4wkhd7ua26oj882253jr4c13k2pe3je18sem69";
 
-  @Bean
-  public JwtDecoder jwtDecoder() {
-    SecretKeySpec secretKey = new SecretKeySpec(this.jwtKey.getBytes(), 0, this.jwtKey.getBytes().length,"RSA");
-    return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
-  }
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		SecretKeySpec secretKey = new SecretKeySpec(this.jwtKey.getBytes(), 0, this.jwtKey.getBytes().length,"RSA");
+		return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
+	}
 
-  @Bean
+	@Bean
 	public JwtEncoder jwtEncoder() {
 		return new NimbusJwtEncoder(new ImmutableSecret<>(this.jwtKey.getBytes()));
 	}
