21abbd2699af2036fa463a45e50d4a8e140815b4
Chen <chenraptor@outlook.fr>
2024-05-02 23:26:18 +0200

    baseprod7

--- src/pkdxapi/src/main/java/fr/iut/ab/pkdxapi/services/CustomUserDetailsService.java
+++ src/pkdxapi/src/main/java/fr/iut/ab/pkdxapi/services/CustomUserDetailsService.java
@@ -5,15 +5,17 @@ import java.util.Optional;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import fr.iut.ab.pkdxapi.models.user.UserData;
 import fr.iut.ab.pkdxapi.repositories.UserRepository;
 
-import org.springframework.security.core.userdetails.User;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private UserRepository userRepository;
